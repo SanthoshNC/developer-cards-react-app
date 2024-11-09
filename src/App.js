c6fcf5a9a4291b286396eb8a884c8f1ef88f1ef9
SOWMIYA B <132748738+SOWMIYA-BALACHANDER@users.noreply.github.com>
2024-11-09 15:13:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to the Developer Cards App</h1>
-      <p>22CB055,SOWMIYA B,<code>developers.json</code> file.</p>
+      <p>22CB055,<code>developers.json</code> file.</p>
       <CardList />
     </div>
   );
